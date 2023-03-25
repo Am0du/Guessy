6007daea446a70398d319373b9c6f069f6065e44
Amodu David <104693505+Am0du@users.noreply.github.com>
2023-03-25 00:51:48 +0000

    added a Docstring to my functions and also added an input function that gets the player's name.

--- main.py
+++ main.py
@@ -11,9 +11,10 @@ import random
 from replit import clear
 from art import logo
 
-def check_guess(comp_guess, user_guess):
+def check_guess(comp_guess, user_guess, user_name):
+    """Checks if the player guess the right number"""
     if user_guess == comp_guess:
-        return f"Correct!!! The computer guessed {comp_guess}"
+        return f"Correct!!! The computer guessed {comp_guess}, {user_name} wins"
     elif user_guess > comp_guess:
         return "Too high"
     elif user_guess < comp_guess:
@@ -21,6 +22,7 @@ def check_guess(comp_guess, user_guess):
 
 
 def game_level(user_level):
+    """Checks the level of difficult the player inputed"""
     if user_level == 'hard':
         player_life = 5
         return player_life
@@ -36,6 +38,7 @@ def game_logo():
     
 
 print(f"{logo}\nWelcome to the Number Guessing game!\nI'm thinking of a number between 1 and 100")
+user_name = input("What's your name: ")
 diff_level = input("Choose a difficulty. Type 'easy' or 'hard': ")
 
 comp_ = random.randint(1, 100)
@@ -47,7 +50,7 @@ if game_level(diff_level):
         print(f"You've have {player_life} life remaining to get the answer")
         user_ = int(input("Make a guess: "))
         
-        print(check_guess(comp_, user_))
+        print(check_guess(comp_, user_, user_name))
         player_life -= 1
            
         user_choice = input("Do you want to stop, Type 'yes' to stop and 'no' to continue: ").lower()
@@ -60,7 +63,7 @@ if game_level(diff_level):
         print("you've used up your life\nGame over!!!!!")
     elif user_choice =='yes':
         game_logo()
-        print(f"Your guess is {check_guess(comp_, user_)}")
+        print(f"Your guess is {check_guess(comp_, user_, user_name)}")
         
 
 
